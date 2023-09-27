eacb39c87bfc7ad8ac2ee2c7d4e42c7c8c9f31d0
iVladyuser <korol.v09@gmail.com>
2023-09-27 14:10:47 +0200

    next commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './js/header/header';
 import './js/hero/hero';
+import './js/cocktailsMarkUp/cocktailsMarkUp';
 import './js/searchCocktails/searchCocktails';
 import './js/modalCocktails/modalCocktails';
