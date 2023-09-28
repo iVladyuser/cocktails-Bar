619ae5b0d020de03db704910e092f4a66c7ac618
iVladyuser <129949946+iVladyuser@users.noreply.github.com>
2023-09-28 11:07:42 +0000

    Deploying to gh-pages from @ iVladyuser/cocktails-Bar@90393e6d875fd9fb05c0d087455a31097c5baa85 🚀

--- assets/index-706817f7.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-ec808ebb.js";console.log(5);
---
+++ assets/index-9ea2c466.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-ec808ebb.js";console.log(5);const e={searchInput:document.querySelector('input[name="searchQuery"'),searchForm:document.getElementById("search-form")};e.searchForm.addEventListener("submit",onFormSubmit);
