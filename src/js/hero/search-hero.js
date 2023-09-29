c6c46df29d3941f7ea01c69896489cf5631c8a24
Max-IT-ZT <babuskomax@gmail.com>
2023-09-29 14:35:55 +0300

    fix-search.js

--- src/js/hero/search-hero.js
+++ src/js/hero/search-hero.js
@@ -1,5 +1,3 @@
-js - keypres;
-
 const keyboard = [
   65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
   84, 85, 86, 87, 88, 89, 90, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48,
@@ -76,5 +74,3 @@ document
 
 const textInput = document.querySelector('.search-js');
 const Btn = document.querySelectorAll('.keyboard-letter');
-
-main;
