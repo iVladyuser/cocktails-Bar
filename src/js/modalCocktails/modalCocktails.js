611f1874cb96d89970294c8a88e7c8b418a29349
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-10-01 00:51:37 +0300

    update modal cocktail

--- src/js/modalCocktails/modalCocktails.js
+++ src/js/modalCocktails/modalCocktails.js
@@ -1,78 +1,59 @@
-import { fetchCocktailGallery } from '../cocktailsMarkUp/cocktailsMarkUp'
-import { saveToFavorites, removeFromFavorites } from '../api/api';
+import axios from 'axios';
+import { BASE_URL } from '../api/api';
 
 const modalCocktailContent = document.querySelector('.modal-cocktail__content');
-const backDrop = document.querySelector('#modal-cocktail');
+const modalCocktail = document.querySelector('#modal-cocktail');
+// modalCocktail.classList.remove('is-hidden');
 
-const onClickModal = createOnClickForModal(
-  saveToFavorites,
-  removeFromFavorites
-);
+async function fetchCocktail() {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/cocktails/lookup/?id=639b6de9ff77d221f190c57e`
+    );
+    const data = response.data;
 
+    renderCocktailList(data, modalCocktailContent);
 
-async function renderCocktailCard() {
-  const {
-    strIngredient1,
-    strIngredient2,
-    strIngredient3,
-    strIngredient4,
-    strIngredient5,
-    strIngredient6,
-    strIngredient7,
-    strIngredient8,
-    strIngredient9,
-    strIngredient10,
-    strIngredient11,
-    strIngredient12,
-    strIngredient13,
-    strIngredient14,
-    strIngredient15,
-  } = await fetchCocktailGallery();
-  const ingredients = [
-    strIngredient1,
-    strIngredient2,
-    strIngredient3,
-    strIngredient4,
-    strIngredient5,
-    strIngredient6,
-    strIngredient7,
-    strIngredient8,
-    strIngredient9,
-    strIngredient10,
-    strIngredient11,
-    strIngredient12,
-    strIngredient13,
-    strIngredient14,
-    strIngredient15,
-  ].filter(item => item);
+    console.log(data[0]);
+  } catch (error) {
+    console.error('Error while getting cocktail:', error);
+    throw error;
+  }
+}
 
-  const markup = /*html*/ `
+// fetchCocktail();
+
+const renderCocktailList = (arr, container) => {
+  const markup = /*html*/ arr
+    .map(
+      item => `
       <div class="modal-header">
-        <img class="modal-header__img" src="${item.DrinkThumb}" alt="${item.drink}" loading="lazy" width="280" height="280" />
+        <img class="modal-header__img" src="${item.drinkThumb}" alt="${item.drink}" loading="lazy" width="280" height="280" />
         <div class="modal-header__heading">
           <h2 class="modal-header__title">${item.drink}</h2>
-          <h3 class="modal-header__subtitle">ingredients</h3>
+          <h3 class="modal-header__subtitle">Ingredients</h3>
           <p class="modal-header__text">Per cocktail</p>
           <ul class="modal-header__list">
             ${ingredients
-              .map(
-                item =>
-                  `<li class="modal-header__item">
-                    <a href="#" class="modal-header__link" data-ingredient="${item}">✶ ${item}</a>
+          .map(
+            item =>
+              `<li class="modal-header__item">
+                    <a href="#" class="modal-header__link" data-ingredient="${item.title}">✶ ${item.title}</a>
                   </li>`
-              )
-              .join('')}
+          )
+          .join('')}
           </ul>
         </div>
       </div>
       <div class="modal-header--bottom">
-      <h3 class="modal-header__subtitle">instractions:</h3>
+      <h3 class="modal-header__subtitle">Instractions:</h3>
       <p class="modal-header__text">${item.description}</p>
       </div>
-       `;
-  modalCocktailContent.innerHTML = markup;
-  backDrop.classList.remove('is-hidden');
+       `
+    )
+    .join('');
 
-}
+  container.insertAdjacentHTML('afterbegin', markup);
+              
+};
 
-export { renderCocktailCard };
