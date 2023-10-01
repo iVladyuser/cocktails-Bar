cce1ff50b1bd2173879e4a95865731a260067546
GusarovaYuliya <gusarovayuliya25@gmail.com>
2023-10-01 13:07:58 +0300

    comm

--- src/js/hero/search-hero.js
+++ src/js/hero/search-hero.js
@@ -6,13 +6,45 @@ import { getCocktailsByLetter } from '../api/api'
 import { searchCocktailsByName } from '../api/api'
 import { head } from 'lodash';
 import { KEY_CODES } from 'choices.js';
-// import { renderList } from '../cocktailsMarkUp/cocktailsMarkUp'
-
 
 const keyboard = [
   65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
   84, 85, 86, 87, 88, 89, 90, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48,
 ];
+
+
+function init() {
+  let out = '';
+  for (let i = 0; i < keyboard.length; i++) {
+    out +=
+      '<li><button type="button" valeu="' +
+      String.fromCharCode(keyboard[i]) +
+      '"  class="keyboard-letter" data="' +
+      keyboard[i] +
+      '" >' +
+      String.fromCharCode(keyboard[i]) +
+      '</button></li>';
+  }
+  document.querySelector('#keyboard').innerHTML = out;
+}
+init();
+
+
+const keyBoard = document.querySelector(".js-keyboard");
+
+keyBoard.addEventListener("click", (e) => {
+    if(e.target.nodeName  !== "BUTTON"){
+        return;
+    }
+        
+  let button = document.querySelector(".keyboard-letter");
+  
+   console.log(button);
+        const name = button.textContent;
+        console.log(name);
+        searchCocktailsByName(name);
+});
+
 
 // ПО МАСИВУ ШУКАВ КОДИ КЛАВІАТУРИ
 
@@ -22,78 +54,40 @@ const keyboard = [
 //     console.log(keyboard);
 // }
 
-//-------------------------------max 768
-function inity() {
-  let out = '';
-  for (let i = 0; i < keyboard.length; i++) {
-    out +=
-      '<option  data="' +
-      keyboard[i] +
-      '" class="hero-mobyle-min">' +
-      String.fromCharCode(keyboard[i]) +
-      '</option>';
-  }
-  document.querySelector('#keyboardmin').innerHTML = out;
-}
-inity();
-
-
-
-// const setOption = document.querySelector(".hero-mobyle-min");
-
-// setOption.addEventListener("change", getCocktailsByLetter);
-
-
-
-
 
 
 //-------------------------------min 768
 
-function init() {
-  let out = '';
-  for (let i = 0; i < keyboard.length; i++) {
-    out +=
-      '<button type="button"   class="keyboard-letter" data="' +
-      keyboard[i] +
-      '" >' +
-      String.fromCharCode(keyboard[i]) +
-      '</button>';
-  }
-  document.querySelector('#keyboard').innerHTML = out;
-}
-init();
+// function init() {
+//   let out = '';
+//   for (let i = 0; i < keyboard.length; i++) {
+//     out +=
+//       '<button type="button"  class="keyboard-letter" data="' +
+//       keyboard[i] +
+//       '" >' +
+//       String.fromCharCode(keyboard[i]) +
+//       '</button>';
+//   }
+//   document.querySelector('#keyboard').innerHTML = out;
+// }
+// init();
 
-
-document.querySelectorAll('#keyboard .keyboard-letter')
-  .forEach(function (element) {
-    element.onclick = function (event) {
-      document
-        .querySelectorAll('#keyboard .keyboard-letter')
-        .forEach(function (element) {
-          element.classList.remove('active');
-        });
-      let code = this.getAttribute('data');
-      this.classList.add('active');
-    };
-  });
-
-
+// document
+//   .querySelectorAll('#keyboard .keyboard-letter')
+//   .forEach(function (element) {
+//     element.onclick = function (event) {
+//       document
+//         .querySelectorAll('#keyboard .keyboard-letter')
+//         .forEach(function (element) {
+//           element.classList.remove('active');
+//         });
+//       let code = this.getAttribute('data');
+//       this.classList.add('active');
+//     };
+//   });
 
 // БУКВА В ПОШУК
 
-const keyBoard = document.querySelector(".js-keyboard");
-let letterOrNumber = keyBoard.querySelectorAll('button[dataset]');
-console.log(letterOrNumber);
-// keyBoard.querySelectorAll(".keyboard-letter").forEach(function(btn)
-// {btn.addEventListener("click", getCocktailsByLetter(letterOrNumber))});
-keyBoard.addEventListener("click", getCocktailsByLetter(letterOrNumber));
+const textInput = document.querySelector('.search-js');
+const Btn = document.querySelectorAll('.keyboard-letter');
 
-
-
-
-// INPUT
-// const textInput = document.querySelector(".search-js");
-// let input = document.querySelector('input');
-// const name = input.value;
-// input.addEventListener("input", searchCocktailsByName(name));
