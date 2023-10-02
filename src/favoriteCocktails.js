a827e9f3b3c46bc3f6b9227c841d4b1f7083f1dc
Serhii-Bondariev <xx.code4u@gmail.com>
2023-10-02 15:46:25 +0300

    Update favoriteCocktails.js

--- src/favoriteCocktails.js
+++ src/favoriteCocktails.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import Notiflix from 'notiflix'; // Імпортуємо бібліотеку Notiflix для сповіщень
+import Notiflix from 'notiflix';
 import { BASE_URL } from '../api/api';
 import { fetchCocktail } from '../modalCocktails/modalCocktails';
-import { saveToFavorites, removeFromFavorites } from './js/api/api'; // Виправлений імпорт шляху
+import { saveToFavorites, removeFromFavorites } from './js/api/api'; // 
 import { renderList } from './js/hero/hero';
 
 document.addEventListener('DOMContentLoaded', () => {
