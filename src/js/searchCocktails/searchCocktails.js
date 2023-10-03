d2228b8423c0b8db9da3b7154f356ee444f30949
iVladyuser <korol.v09@gmail.com>
2023-10-03 23:33:30 +0200

    dd

--- src/js/searchCocktails/searchCocktails.js
+++ src/js/searchCocktails/searchCocktails.js
@@ -1,57 +1,57 @@
-// import Pagination from 'tui-pagination';
-// import { renderList } from '../cocktailsMarkUp/cocktailsMarkUp';
+import Pagination from 'tui-pagination';
+import { renderList } from '../cocktailsMarkUp/cocktailsMarkUp';
 
-// const galleryCocktails = document.querySelector('.cocktails-list');
+const galleryCocktails = document.querySelector('.cocktails-list');
 
-// export function pagination(cocktailData) {
-//   function isMobile() {
-//     if (window.innerWidth >= 768) {
-//       return false;
-//     } else {
-//       return true;
-//     }
-//   }
-//   if (window.innerWidth >= 1280) {
-//     cardsPerPage = 9;
-//   } else {
-//     cardsPerPage = 8;
-//   }
-//   const itemsPerPage = cardsPerPage; // Кількість елементів на сторінці
-//   console.log(cardsPerPage);
-//   let currentPage = 1; // Поточна сторінка
-//   const totalItems = cocktailData.length; // Загальна кількість елементів
+export function pagination(cocktailData) {
+  function isMobile() {
+    if (window.innerWidth >= 768) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  if (window.innerWidth >= 1280) {
+    cardsPerPage = 9;
+  } else {
+    cardsPerPage = 8;
+  }
+  const itemsPerPage = cardsPerPage; // Кількість елементів на сторінці
+  console.log(cardsPerPage);
+  let currentPage = 1; // Поточна сторінка
+  const totalItems = cocktailData.length; // Загальна кількість елементів
 
-//   const container = document.getElementById('pagination');
+  const container = document.getElementById('pagination');
 
-//   // Опції для пагінатора
-//   const options = {
-//     totalItems,
-//     itemsPerPage,
-//     visiblePages: 5, // Кількість видимих сторінок в пагінаторе
-//     centerAlign: true, // Рівняння по центру
-//   };
+  // Опції для пагінатора
+  const options = {
+    totalItems,
+    itemsPerPage,
+    visiblePages: 5, // Кількість видимих сторінок в пагінаторе
+    centerAlign: true, // Рівняння по центру
+  };
 
-//   const pagination = new Pagination(container, options);
+  const pagination = new Pagination(container, options);
 
-//   // Обробник зміни сторінки
-//   pagination.on('beforeMove', event => {
-//     currentPage = event.page;
-//     renderPage();
-//   });
+  // Обробник зміни сторінки
+  pagination.on('beforeMove', event => {
+    currentPage = event.page;
+    renderPage();
+  });
 
-//   //Функція для відображення данних на сторінці
-//   function renderPage() {
-//     const startIndex = (currentPage - 1) * itemsPerPage;
-//     const endIndex = startIndex + itemsPerPage;
-//     const paginatedData = cocktailData.slice(startIndex, endIndex);
+  //Функція для відображення данних на сторінці
+  function renderPage() {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const paginatedData = cocktailData.slice(startIndex, endIndex);
 
-//     // Тут вставляти розмітку на сторінку, рендерити картки
-//     console.log(paginatedData);
-//     renderList();
-//   }
+    // Тут вставляти розмітку на сторінку, рендерити картки
+    console.log(paginatedData);
+    renderList();
+  }
 
-//   // Ініціалізація початкової сторінки
-//   fetchCocktailGallery();
-// }
+  // Ініціалізація початкової сторінки
+  fetchCocktailGallery();
+}
 
 
