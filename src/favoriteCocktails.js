72f6b00bc7e5761574f002f606d5e4f339f0a52a
ciplator <ciplator@gmail.com>
2023-10-06 16:54:45 +0300

    Update favoriteCocktails.js

--- src/favoriteCocktails.js
+++ src/favoriteCocktails.js
@@ -3,7 +3,7 @@ import {
   closeModal,
   cocktailsArray,
 } from './js/modalCocktails/modalCocktails';
-
+import spriteRafiki from '/img/sprite.svg'
 const KEY_FAVORITE = 'favoriteCocktails';
 const favoriteCocktailList = document.querySelector(
   '.add-to-favorite-coctail-list'
@@ -18,7 +18,7 @@ const renderFavoriteCocktail = async (arr, container) => {
            <div class="container">
         <div class="block-page favorite-block-page">
           <svg class="icon-rafiki" >
-          <use href="/img/sprite.svg#icon-rafiki"></use>
+          <use href="${spriteRafiki}#icon-rafiki"></use>
         </svg>
           <p class="text-page">
             You haven't added any <br /><span class="text-span"
