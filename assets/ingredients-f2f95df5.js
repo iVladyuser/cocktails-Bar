3302a00d673c0b56d5dda7ff09c6bd2fa3b60608
iVladyuser <129949946+iVladyuser@users.noreply.github.com>
2023-10-06 09:35:35 +0000

    Deploying to gh-pages from @ iVladyuser/cocktails-Bar@11b64c9b01b7dcf50656bd74c47b794a4325bb89 🚀

--- assets/ingredients-f2f95df5.js
+++ assets/ingredients-f2f95df5.js
@@ -1,4 +1,4 @@
-import{i as u,d as k,a as f}from"./header-c41f932f.js";const p=document.querySelector("#modal-cocktail"),S=document.querySelector(".modal");document.querySelector(".modal-cocktail__content");const y=document.querySelector(".modal-ingredients__content"),i="favoriteIngredients",l=document.querySelector(".add-to-favorite-list"),I=JSON.parse(localStorage.getItem(i))??[],c=async(e,n)=>{const r=document.querySelector(".add-to-favorite-list");if(e.length===0)r.innerHTML=`
+import{i as u,d as k,a as f}from"./header-e4588436.js";const p=document.querySelector("#modal-cocktail"),S=document.querySelector(".modal");document.querySelector(".modal-cocktail__content");const y=document.querySelector(".modal-ingredients__content"),i="favoriteIngredients",l=document.querySelector(".add-to-favorite-list"),I=JSON.parse(localStorage.getItem(i))??[],c=async(e,n)=>{const r=document.querySelector(".add-to-favorite-list");if(e.length===0)r.innerHTML=`
       <div class="block-page-hero favorite-block-page">
         <svg class="icon-rafiki" >
           <use href="./img/sprite.svg#icon-rafiki"></use>
